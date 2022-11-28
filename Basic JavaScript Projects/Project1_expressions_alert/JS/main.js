8bbf9477dd978464e4b121e3a05b93205531483f
Dar1ux <djankaus87@gmail.com>
2022-11-28 11:23:15 +0000

    Assignment Submission # 1
    Assignment Submission # 1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+var A = "\"Hello World!\"" + " said a programmer." // Defining variable and giving it a string value
+var firstName = "Rob", surname = "Jones", profession = "Astronaut", age = "45 years old" // Defining variables and giving it a string values
+
+firstName = firstName.fontcolor("yellow"); // Assigning color to a string using fontcolor method
+surname = surname.fontcolor("green"); // Assigning color to a string using fontcolor method
+profession = profession.fontcolor("red"); // Assigning color to a string using fontcolor method
+age = age.fontcolor("pink"); // Assigning color to a string using fontcolor method
+
+window.alert(A); // Printing value of A variable into alert message box
+
+document.write(firstName + "<br>"); // Printing variable in an HTML document and moving to a new line
+document.write(surname + "<br>"); // Printing variable in an HTML document and moving to a new line
+document.write(profession + "<br>"); // Printing variable in an HTML document and moving to a new line
+document.write(age + "<br>"); // Printing variable in an HTML document and moving to a new line
+document.write(3 + 3); // Printing value of an expression to an HTML document
