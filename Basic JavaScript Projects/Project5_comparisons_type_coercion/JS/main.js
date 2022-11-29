579751a0ed993c996c32cf956932b09b29fff58d
Dar1ux <djankaus87@gmail.com>
2022-11-29 11:52:17 +0000

    Assignment Submission # 5
    Assignment Submission # 5

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,11 @@
+function testOperators() { // Defining a function and giving it a name
+    document.write(typeof 0.5 + "<br>"); // Using typeof operator and displaying variable's data type in a browser
+    document.write(("10" + 5) + "<br>"); // Expression combining string and a number displayed in a browser
+    document.write((10 == 10) + "<br>"); // Using == comparison operator and displaying output in a browser
+    document.write((5 === "5") + "<br>"); // Using === comparison operator and displaying output in a browser
+    document.write((5 > 8) + "<br>"); // Using > comparison operator and displaying output in a browser
+    document.write((2 < 5) + "<br>"); // Using < comparison operator and displaying output in a browser
+    document.write((5 > 1 && 6 > 5) + "<br>"); // Using && (AND) Boolean logic operator and displaying output in a browser
+    document.write((1 > 5 || 6 > 5) + "<br>"); // Using || (OR) Boolean logic operator and displaying output in a browser
+    document.write(!(10 == 10) + "<br>"); // Using ! (NOT) Boolean logic operator and displaying output in a browser
+}
